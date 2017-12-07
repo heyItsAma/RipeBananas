ed76f2287adcac8d884fe51f9449337eab7ff4a2
Ama Freeman <ama.freeman96@gmail.com>
2017-12-07 18:30:27 -0500

    RipeBananas with Database (Add only)

---
+++ app/src/main/java/com/example/tehhuzzlenut/ripebananas/DatabaseHelper.java
@@ -0,0 +1,73 @@
+package com.example.tehhuzzlenut.ripebananas;
+
+import android.content.ContentValues;
+import android.content.Context;
+import android.database.Cursor;
+import android.database.DatabaseErrorHandler;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteOpenHelper;
+import android.util.Log;
+
+/**
+ * Created by TehHuzzlenut on 12/7/2017.
+ */
+
+public class DatabaseHelper extends SQLiteOpenHelper {
+
+    private static final String TAG = "DatabaseHelper";
+
+    private static final String TABLE_NAME = "food_table";
+
+    //Table Details
+    private static final String ColFoodID = "ID";
+    private static final String ColFoodName = "foodName";
+    private static final String ColFoodExpDate = "foodExpDate";
+
+    public DatabaseHelper(Context context) {
+        super(context, TABLE_NAME, null, 1);
+    }
+
+    @Override
+    public void onCreate(SQLiteDatabase sqLiteDatabase) {
+        String createTable = "CREATE TABLE " + TABLE_NAME + " (ID INTEGER PRIMARY KEY AUTOINCREMENT, " +
+                ColFoodName + " TEXT, " + ColFoodExpDate + " TEXT)";
+        sqLiteDatabase.execSQL(createTable);
+    }
+
+    @Override
+    public void onUpgrade(SQLiteDatabase sqLiteDatabase, int i, int i1) {
+        sqLiteDatabase.execSQL("DROP TABLE IF EXISTS '" + TABLE_NAME + "'");
+    }
+
+    public boolean addData(String item1, String item2){
+        SQLiteDatabase db = this.getWritableDatabase();
+        ContentValues contentValues = new ContentValues();
+        contentValues.put(ColFoodName, item1);
+        contentValues.put(ColFoodExpDate, item2);
+
+        Log.d(TAG, "addData: Adding " + item1 + " and " + item2 + " to " + TABLE_NAME);
+
+        long result = db.insert(TABLE_NAME, null, contentValues);
+
+        //If data is inserted incorrectly, it will return
+        if (result == -1){
+            return false;
+        } else {
+            return true;
+        }
+    }
+
+    //Cursor to return database data
+    public Cursor getData(){
+        Log.d(TAG, "getData: got into function.");
+        SQLiteDatabase db = this.getWritableDatabase();
+
+        Log.d(TAG, "getData: got database.");
+        String query = "SELECT * FROM " + TABLE_NAME;
+        Log.d(TAG, "getData: made query.");
+        Cursor data = db.rawQuery(query, null);
+        Log.d(TAG, "getData: made cursor and reutrn.");
+        return data;
+    }
+
+}
